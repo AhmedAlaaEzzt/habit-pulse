b6e785d5b7b922fff06e7b6af3cf5bb2145c1cfc
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2024-06-26 21:54:44 +0300

    create map centered on Jabal Ali

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useRef } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+
 import "./App.css";
 
 function App() {
-  return <div>I'm going to build a map here!</div>;
+  const viewDivRef = useRef(null);
+  useEffect(() => {
+    if (viewDivRef.current) {
+      const map = new Map({
+        basemap: "topo-vector",
+      });
+
+      new MapView({
+        container: viewDivRef.current as HTMLDivElement,
+        map,
+        zoom: 13,
+        center: [55, 25], // longitude, latitude
+      });
+    }
+  }, []);
+  return <div className="viewDiv" ref={viewDivRef} />;
 }
 export default App;

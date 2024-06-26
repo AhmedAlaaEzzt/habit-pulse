b7f6e2852a2c7b1581a60442d59e0b9e4fcfb120
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2024-06-26 21:10:30 +0300

    remove bootstrap and clear the solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
-import { Header } from "./components";
 
 function App() {
-  return (
-    <>
-      <Header />
-      Hello v1
-    </>
-  );
+  return <div>I'm going to build a map here!</div>;
 }
-
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 {
   /* The following line can be included in your src/index.js or App.js file */
 }
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App.tsx";
 import "./index.css";
 
